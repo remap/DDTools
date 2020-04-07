86d2fa2dab1b3dc0f5efcc1619c526ca62fa300b
peetonn <gpeetonn@gmail.com>
2020-04-07 16:00:34 -0700

    saving base world for the game in DDManager

--- Source/DDManager/Private/DDManager.cpp
+++ Source/DDManager/Private/DDManager.cpp
@@ -50,8 +50,14 @@ void FDDModuleManager::onPostWorldCreation(UWorld *world)
 {
     if (world)
     {
-//        DLOG_TRACE("map name: {}", TCHAR_TO_ANSI(*world->GetMapName()));
-        ((FDDModuleManager*)FDDModuleManager::getSharedInstance())->lastWorldCreated_ = world;
+        if (!world->GetMapName().Equals(FString("Untitled_0")))
+        {
+            if (!((FDDModuleManager*)FDDModuleManager::getSharedInstance())->lastWorldCreated_)
+            {
+                DLOG_TRACE("set base world: {}", TCHAR_TO_ANSI(*world->GetMapName()));
+                ((FDDModuleManager*)FDDModuleManager::getSharedInstance())->lastWorldCreated_ = world;
+            }
+        }
         ((FDDModuleManager*)FDDModuleManager::getSharedInstance())->notifyPostWorldCreation(world);
     }
     else
